a36b7455b8b181223c89f9ee3a878de491f939b9
anubhardwaj2193 <anubhardwaj2193>
2023-06-07 00:31:25 +0530

    locationofexpenditure commit

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,9 +1,20 @@
+import './ExpenseItem.css';
 function ExpenseItem(){
+    const expenseDate=new Date(2021,2,28);
+    const expenseTitle='CAR INSURANCE';
+    const expenseAmount=2000;
+    const expenseLocationOfexpenditure='Honda company,New York';
     return (
-    <div>
-        <div>Food Rs 10</div>
-        <div>Petrol Rs 100</div>
-            <div>Movies Rs 200</div>
+    <div className='expense-item'>
+        <div>{expenseDate.toISOString()}</div>
+       
+        <div className='expense-item__description'>
+            <h2>{expenseTitle}</h2>
+            <div className='expense-item__location'>
+                <h2>{expenseLocationOfexpenditure}</h2></div>
+            <div className='expense-item__price'>Rs{expenseAmount}</div>
+
+</div>
     
     </div>
     );
